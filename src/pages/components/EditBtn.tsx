9eba80f58ef8dc2da71535bcc80f0cc1a2e66b5f
yanasekenta <ynsknt0505@gmail.com>
2022-07-24 14:33:25 +0900

    編集ボタンを作成

---
+++ src/pages/components/EditBtn.tsx
@@ -0,0 +1,11 @@
+import { Button, Icon } from "@chakra-ui/react"
+import { EditIcon } from "@chakra-ui/icons"
+const EditBtn = () => {
+	return (
+  <Button size="xs" ml="5" mb="1" colorScheme="blue">
+    <EditIcon/>
+  </Button>
+  )
+}
+
+export default EditBtn
